5aae39fddb56c5ba86cae5879ebca7e44e5e2823
GitHub Action <action@github.com>
2023-05-05 10:53:06 +0000

    Update version to 0.0.4

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from version_util import VersionUtil
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 version_util = VersionUtil(__version__)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="remla-lib-release",
-    version="0.0.3",
+    version="0.0.4",
     description="A version-aware library for REMLA12 project",
     long_description=long_description,
     long_description_content_type="text/markdown",
