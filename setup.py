e90a85c5057c91da16b831bc69512584bcf60da2
Nektarios Evangelou <nekkieee@hotmail.com>
2023-05-05 13:05:42 +0200

    Added the workflows and automatic versioning

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="remla12-lib-release",
+    version="0.0.1",
+    description="A version-aware library for REMLA12 project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/remla23-team12/lib",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
---
+++ version_util.py
@@ -0,0 +1,6 @@
+class VersionUtil:
+    def __init__(self, version):
+        self.version = version
+
+    def get_version(self):
+        return self.version
