a13207add1b3b54b06c9617f88851faf3967a05f
ZihaoXu <z.xu@student.utwente.nl>
2023-05-24 19:24:22 +0200

    0.1.5

--- remla12_lib_release/__init__.py
+++ remla12_lib_release/__init__.py
@@ -1,4 +1,4 @@
-from lib.remla12_lib_release.version_util import VersionUtil
+from .version_util import VersionUtil
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 version_util = VersionUtil(__version__)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="remla12_lib_release",
-    version="0.1.4",
+    version="0.1.5",
     description="A version-aware library for REMLA12 project",
     long_description=long_description,
     long_description_content_type="text/markdown",
