6ec2289c7bcc03c4d2971d56307bd73bf63a3ac3
GitHub Action <action@github.com>
2023-05-24 13:55:43 +0000

    Update version to 0.0.8

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from version_util import VersionUtil
 
-__version__ = "0.0.6"
+__version__ = "0.0.8"
 version_util = VersionUtil(__version__)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="remla12-lib-release",
-    version="0.0.6",
+    version="0.0.8",
     description="A version-aware library for REMLA12 project",
     long_description=long_description,
     long_description_content_type="text/markdown",
