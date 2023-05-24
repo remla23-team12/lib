870442e00a1601341c40faca06facb95a6815099
ZihaoXu <z.xu@student.utwente.nl>
2023-05-24 19:19:35 +0200

    refactoring

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from version_util import VersionUtil
-
-__version__ = "0.1.3"
-version_util = VersionUtil(__version__)
---
+++ remla12_lib_release/__init__.py
@@ -0,0 +1,4 @@
+from lib.remla12_lib_release.version_util import VersionUtil
+
+__version__ = "0.1.4"
+version_util = VersionUtil(__version__)
